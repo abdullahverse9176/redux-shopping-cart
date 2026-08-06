2f1e00060525f3d011fb0121f21bc4d6fbdd2050
Muhammad Abdullah <abdullah.webdev9176@gmail.com>
2026-08-06 10:47:40 +0500

    updated text

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <main className="max-w-4xl w-full mx-auto mt-5 px-4 font-sans">
-      <h1 className="text-center text-2xl mb-5">Next.js + Zustand E-Commerce</h1>
+      <h1 className="text-center text-2xl mb-5">Next.js + Redux Toolkit E-Commerce</h1>
       <div className="flex gap-4 justify-between">
         <div className="w-1/2">
           <ProductList />
